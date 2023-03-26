e3f29353aaba6c47ab1999ec0a679cb460fe6d19
thomasop <tdss33@hotmail.com>
2023-03-26 14:47:26 +0200

    Add: interface with button and timer and input

--- chrono/ContentView.swift
+++ chrono/ContentView.swift
@@ -6,14 +6,58 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State private var travail: Int = 0
+    @State private var pause: Int = 0
+    @State private var didTap:Bool = false
+    
+    private static let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Pomodoro timer").font(.title)
+                .padding(.top, 40)
+            Text("Choissisez votre timer de travail et de pause").padding(10)
+            HStack {
+                VStack {
+                    Text("Travail")
+                    TextField("Travail", value: $travail, formatter: NumberFormatter())
+                }
+                VStack {
+                    Text("Pause")
+                    TextField("Pause", value: $pause, formatter: NumberFormatter())
+                }
+                
+            }
+            .padding(.horizontal, 80.0)
+            .padding(.top, 50.0)
+            .padding(.bottom, 40.0)
+            HStack {
+                VStack {
+                    
+                    Text("00:00:00")
+                    Button(action: {
+                        if(self.didTap == false) {
+                            self.didTap = true
+                        } else {
+                            self.didTap = false
+                        }
+                    }) {
+                        Text("Start")
+                    }.padding(10)
+                        .background(didTap ? Color.blue : Color.yellow)
+                }
+            }
+            HStack {
+            }
+            .padding(.bottom, 80.0)
+            
         }
         .padding()
     }
