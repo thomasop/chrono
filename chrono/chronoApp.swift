f6ceff689b76d4307fa0554faa2ba360757c15f5
thomasop <tdss33@hotmail.com>
2023-03-26 13:15:52 +0200

    Initial Commit

---
+++ chrono/chronoApp.swift
@@ -0,0 +1,17 @@
+//
+//  chronoApp.swift
+//  chrono
+//
+//  Created by Thomas Da Silva on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct chronoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
