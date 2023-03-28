2268356f2e5cb0c0bc021002f249e65f4fdd49d7
thomasop <tdss33@hotmail.com>
2023-03-28 13:44:55 +0200

    Add: reset button

--- chrono/ContentView.swift
+++ chrono/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     @State private var didTap:Bool = false
     @State var isTimerRunning:Bool = false
     @State var status:Bool = false
-    @State private var time: Int = 0
+    @State private var time: String = ""
     @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     private static let formatter: NumberFormatter = {
@@ -28,7 +28,6 @@ struct ContentView: View {
         VStack {
             Text("Pomodoro timer").font(.title)
                 .padding(.top, 40)
-            Text("Choissisez votre timer de travail et de pause").padding(10)
             Form {
                 HStack {
                     TextField("Travail", value: $travail, formatter: NumberFormatter())
@@ -43,11 +42,15 @@ struct ContentView: View {
                     }
                 }
                 Button("Ajouter ce temps") {
-                    time = travail
+                    if (travail != 0 && pause != 0) {
+                        time = String(travail)
+                        DispatchQueue.main.async {
+                            NSApp.keyWindow?.makeFirstResponder(nil)
+                        }
+                    }
                 }
                 
             }
-            
             .padding(.horizontal, 80.0)
             .padding(.top, 50.0)
             .padding(.bottom, 40.0)
@@ -57,21 +60,21 @@ struct ContentView: View {
                     Text("\(time)").onReceive(timer) {
                         _ in
                         if self.isTimerRunning {
-                            if (time == 0 && status == false) {
-                                time = pause
+                            if (Int(time) == 0 && status == false) {
+                                time = String(pause)
                                 status = true
                                 sendNotification(title: "Pause", message: "Le travail est fini maintenant tu peux prendre une petite pause")
-                            } else if (time == 0 && status == true) {
-                                time = travail
+                            } else if (Int(time) == 0 && status == true) {
+                                time = String(travail)
                                 status = false
                                 sendNotification(title: "Travail", message: "La pause est fini maintenant il faut travailler")
                             }
-                            time -= 1
+                            time = String((Int(time) ?? 0) - 1)
                         }
                     }
                     HStack {
                         Button(action: {
-                            if(time > 0) {
+                            if(Int(time) ?? 0 > 0) {
                                 isTimerRunning = true
                             }
                         }) {
@@ -84,8 +87,17 @@ struct ContentView: View {
                             }) {
                                 Text("Stop")
                             }.padding(10).background(Color.red)
+                            Button(action: {
+                                isTimerRunning = false
+                                time = ""
+                                travail = 0
+                                pause = 0
+                            }) {
+                                Text("Reset")
+                            }.padding(10).background(Color.red)
                         }
                     }
+                    .padding(.bottom, 30)
                 }
             }
             
@@ -101,9 +113,6 @@ struct ContentView: View {
         notificationContent.badge = 1
         
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.01, repeats: false)
-        // you could also use...
-        // UNCalendarNotificationTrigger(dateMatching: .init(year: 2022, month: 12, day: 10, hour: 0, minute: 0), repeats: true)
-        
         let req = UNNotificationRequest(identifier: UUID().uuidString, content: notificationContent, trigger: trigger)
         
         UNUserNotificationCenter.current().add(req)
